d01da382a6de4b1bdad4ab88f209cb63ecfdcf3b
Anshusuni <97733665+Anshusuni@users.noreply.github.com>
2025-05-27 12:06:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-d/from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image as keras_image
 from PIL import Image
 import numpy as np
